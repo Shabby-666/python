1cf8a6dca5a980b6732f66cb556446a0c3ca92fc
Shabby-666 <whx775852@outlook.com>
2024-11-16 22:26:13 +0800

    Add files via upload

--- 浏览器.py
+++ 浏览器.py
@@ -1,20 +1,24 @@
+# 导包（无需另外安装，Python自带）
 import webbrowser as web
 import os
+
+# try后面有大用
 try:
+    # 定义一个函数为s()，执行s()时等于执行下面的代码
     def s():
         print("必应（Bing）：1")
         print("百度（Baidu）：2")
         print("360导航：3")
         print("搜狗（Sogou）：4")
         print("中国搜索（ChinaSo）：5")
-        engine = input("请输入你要使用的搜索引擎（退出请按Ctrl+C）：")
-        if engine == "1":
+        engine = input("请输入你要使用的搜索引擎（退出请按Ctrl+C）：") # 询问需要的搜索引擎
+        if engine == "1": #判断用户选择的搜索引擎（下面以此类推）
             while True:
                 search = input("你要搜索的内容（切换搜索引擎输入engines）：")
-                if search == "engines":
+                if search == "engines": # 判断是否切换搜索引擎（下面以此类推）
                     s()
                 else:
-                    web.open(f"https://cn.bing.com/search?q={search}&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQg1MTE1ajBqOagCCLACAQ&FORM=ANAB01&adppc=EdgeStart&PC=CNNDDB")
+                    web.open(f"https://cn.bing.com/search?q={search}&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQg1MTE1ajBqOagCCLACAQ&FORM=ANAB01&adppc=EdgeStart&PC=CNNDDB") # 打开搜索网页（下面以此类推）
         elif engine == "2":
             while True:
                 search = input("你要搜索的内容（切换搜索引擎输入engines）：")
@@ -44,10 +48,13 @@ try:
                 else:
                     web.open(f"https://www.chinaso.com/newssearch/all/allResults?q={search}")
 
+    # 循环执行函数s()
     while True:
         s()
+
+# 捕捉由于用户按下Ctrl+C导致的结束（try立大功）
 except KeyboardInterrupt:
     print("\n已退出，再见")
 finally:
     print("GoodBye")
-    os.system("pause")
+    os.system("pause") # 防止应用自动退出，导致用户看不到“再见”的字样
